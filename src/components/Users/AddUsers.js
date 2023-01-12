99d111a90a80b05a74b75dfab0c0bcab16160695
manish0718 <manishsehgal976@gmail.com>
2023-01-12 00:13:54 -0500

    first commit

---
+++ src/components/Users/AddUsers.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Button from "../UI/Button";
+import Card from "../UI/Card";
+import styles from "./AddUsers.module.css";
+const AddUsers = (props) => {
+  const addUserHandler = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <Card className={styles.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text" />
+        <label htmlFor="age">Age (Years)</label>
+        <input id="age" type="number" />
+        <Button type="submit">Add User</Button>
+      </form>
+    </Card>
+  );
+};
+
+export default AddUsers;
